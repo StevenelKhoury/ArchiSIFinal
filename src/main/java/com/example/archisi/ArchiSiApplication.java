cf5b2c0a3bd8fc34a352f05e77bbc3c9b6a4c7e4
Steven EL Khoury <57746806+StevenelKhoury@users.noreply.github.com>
2022-03-28 23:32:15 +0200

    Add files via upload

---
+++ src/main/java/com/example/archisi/ArchiSiApplication.java
@@ -0,0 +1,13 @@
+package com.example.archisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiSiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiSiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/archisi/ArchiSiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.archisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiSiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
